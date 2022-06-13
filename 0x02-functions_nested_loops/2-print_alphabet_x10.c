a36d33262b968c7c00bebf46ee68652f2d930418
anwar-sultan <anwarsultan64@gmail.com>
2022-06-13 21:09:34 +0300

    print_alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 -> prints alphabet letters
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; x < 10; x++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
