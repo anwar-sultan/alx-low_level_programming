75c662271a84c9b585581a9edbf43bfe6e5a6616
anwar-sultan <anwarsultan64@gmail.com>
2022-06-14 06:23:40 +0300

    complete

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,7 +14,7 @@ int largest_number(int a, int b, int c)
 
 	if (a >= b && a >= c)
 	{
-        	largest = a;
+		largest = a;
 	}
 	else if (b >= a && b >= c)
 	{
@@ -24,7 +24,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
 
